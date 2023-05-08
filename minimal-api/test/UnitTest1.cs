d4da5cb600bb27428aea7f61afbf6b7301af7905
leon123858 <a0970785699@gmail.com>
2023-05-08 15:58:59 +0800

    add unit test

---
+++ minimal-api/test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace test;
+using minimal_api.Services;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup() { }
+
+    [Test]
+    public void Test1()
+    {
+        TodoService service;
+        Assert.AreEqual(4, 4);
+        Assert.Pass();
+    }
+}
